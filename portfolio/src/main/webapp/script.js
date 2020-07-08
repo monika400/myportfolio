944662ec69d64d891347ee8d80358387e0750d45
Monika Rathore <monikarthr87@gmail.com>
2020-07-08 02:04:04 -0700

    Add selfie and basic intro

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -20,7 +20,8 @@ function addRandomGreeting() {
       ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
 
   // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+  //const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+  const greeting = "What's Up Dude!!";
 
   // Add it to the page.
   const greetingContainer = document.getElementById('greeting-container');
